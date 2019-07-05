698c869a623862e88a003a52255e9425f103c4b2
Kate Howard <kate@simpleweb.co.uk>
2019-07-05 17:02:33 +0100

    Add progress bar to songs

--- src/components/Audio.js
+++ src/components/Audio.js
@@ -3,11 +3,14 @@ import React, { useState } from "react";
 function Audio(props) {
   const [played, togglePlayed] = useState(false);
   const [playing, togglePlaying] = useState(false);
+  const [progressLength, setProgressLength] = useState(0);
   const { roundNumber, songNumber, decreaseBonusPoints } = props;
   const song = require(`../music/${roundNumber}/${songNumber}.mp3`);
   let _audio = HTMLAudioElement;
 
   function play() {
+    const { duration, currentTime } = _audio;
+    setProgressLength((currentTime / duration) * 100);
     _audio.play();
     togglePlaying(true);
     if (!played) {
@@ -27,13 +30,21 @@ function Audio(props) {
           ▶
         </button>
       )}
+      <div className="progress">
+        <div
+          style={{
+            backgroundColor: "#279add",
+            width: `${progressLength}%`,
+            height: "10px"
+          }}
+        />
+      </div>
       <audio
         ref={c => (_audio = c)}
+        // This stops songs thinking they've already been played when loading a new round
         onLoadedData={() => togglePlayed(false)}
         onPause={() => togglePlaying(false)}
         onEnded={() => togglePlaying(false)}
-        controls="controls"
-        controlsList="nodownload"
         src={song}
         style={{ display: "none" }}
       />
