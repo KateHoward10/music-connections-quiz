56517a09c15854445b3b2ee31d75b66f2600c302
Kate Howard <kate@simpleweb.co.uk>
2019-07-05 15:52:51 +0100

    Custom audio component

--- src/components/Audio.js
+++ src/components/Audio.js
@@ -2,24 +2,42 @@ import React, { useState } from "react";
 
 function Audio(props) {
   const [played, togglePlayed] = useState(false);
+  const [playing, togglePlaying] = useState(false);
   const { roundNumber, songNumber, decreaseBonusPoints } = props;
   const song = require(`../music/${roundNumber}/${songNumber}.mp3`);
+  let _audio = HTMLAudioElement;
 
   function play() {
+    _audio.play();
+    togglePlaying(true);
     if (!played) {
-      decreaseBonusPoints();
       togglePlayed(true);
+      decreaseBonusPoints();
     }
   }
 
   return (
-    <audio
-      onPlay={play}
-      onLoadedData={() => togglePlayed(false)}
-      controls="controls"
-      controlsList="nodownload"
-      src={song}
-    />
+    <div>
+      {playing ? (
+        <button className="audio-button" onClick={() => _audio.pause()}>
+          ❚❚
+        </button>
+      ) : (
+        <button className="audio-button" onClick={play}>
+          ▶
+        </button>
+      )}
+      <audio
+        ref={c => (_audio = c)}
+        onLoadedData={() => togglePlayed(false)}
+        onPause={() => togglePlaying(false)}
+        onEnded={() => togglePlaying(false)}
+        controls="controls"
+        controlsList="nodownload"
+        src={song}
+        style={{ display: "none" }}
+      />
+    </div>
   );
 }
 
