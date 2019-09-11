ff54af1c7bfa76c9ad7b0d65dad2ce5c95a1150e
Kate Howard <k.l.howard.1992@gmail.com>
2019-09-11 19:17:12 +0100

    Center symbol on play/pause button

--- src/components/Song/styles.js
+++ src/components/Song/styles.js
@@ -60,6 +60,9 @@ export const Button = styled.button`
   height: 40px;
   border-radius: 20px;
   margin-right: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   background-color: #324bd9;
   color: #fff;
   font-size: 20px;
