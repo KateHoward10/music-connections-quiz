4ce9f74ef4acfcd49e973ae23f8f2414f3bdb5be
Kate Howard <k.l.howard.1992@gmail.com>
2021-01-21 18:30:40 +0000

    Reduce width of header content

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,8 +1,12 @@
 import styled from 'styled-components';
 
 export const StyledHeader = styled.header`
-  padding: 4vw;
+  padding: 3vw;
   background: linear-gradient(#011e4c, #324bd9 90%);
   color: #fcfcfc;
 `;
 
+export const Container = styled.div`
+  max-width: 720px;
+  margin: auto;
+`;
